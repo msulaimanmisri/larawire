91ba0dcaaf0432fa247ae3bf6b45462c3563d428
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-11-02 22:16:49 +0800

    LW : Add scripting

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -8,6 +8,8 @@
     <title>LaraWire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    @livewireStyles
 </head>
 
 <body class="bg-light">
@@ -15,7 +17,7 @@
     @include('layouts.page-menu')
     @yield('content')
     @include('layouts.footer')
-
+    @livewireScripts
 </body>
 
 </html>
