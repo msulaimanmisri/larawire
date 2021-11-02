adcb3c5f5d74d36d7ec5e83007b6adc4b99e7c88
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-11-02 22:32:46 +0800

    LW : Add Table at Index

---
+++ app/Http/Livewire/IndexContact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Contact;
+use Livewire\Component;
+
+class IndexContact extends Component
+{
+    public function render()
+    {
+        $contacts = Contact::all();
+        return view('livewire.index-contact')->with('contacts', $contacts);
+    }
+}
