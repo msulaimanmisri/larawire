cf3a4fcd464cdb4c3392bc855634a74716f5d71f
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-11-05 16:07:03 +0800

    modified files

--- resources/views/livewire/index-contact.blade.php
+++ resources/views/livewire/index-contact.blade.php
@@ -1,23 +1,22 @@
-<div>
-    <div class="container">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col"> No </th>
-                    <th scope="col"> Name </th>
-                    <th scope="col"> Email </th>
-                </tr>
-            </thead>
+<div class="container">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col"> No </th>
+                <th scope="col"> Name </th>
+                <th scope="col"> Email </th>
+            </tr>
+        </thead>
 
-            @foreach ($contacts as $contact)
-            <tbody>
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $contact->name }}</td>
-                    <td>{{ $contact->email }}</td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
-    </div>
+        @foreach ($contacts as $contact)
+        <tbody>
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->email }}</td>
+            </tr>
+        </tbody>
+        @endforeach
+    </table>
+</div>
 </div>
